f4cbda39c10b9d65aaeaa1f614ec7d9f550d096d
Vadym Kaninskyi <Vadim.cpp@gmail.com>
2025-07-01 19:40:53 +0200

    Add user profile page

---
+++ app/[lng]/user/profile/signout-button.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+export default function SignOutButton({ label, lng }: { label: string, lng: string }) {
+  const handleClick = () => {
+    if (typeof window !== 'undefined') {
+      localStorage.setItem('authorized', 'false')
+      window.location.href = `/${lng}`
+    }
+  }
+
+  return (
+    <button onClick={handleClick} className='target-action__link'>
+      {label}
+    </button>
+  )
+}
